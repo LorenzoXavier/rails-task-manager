24b98fc943f816ee03a48b8b0d54aab5f740fbb8
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2019-02-08 14:05:32 +0800

    view done

--- db/migrate/20190208042118_add_default_value.rb
+++ db/migrate/20190208042118_add_default_value.rb
@@ -1,6 +1,6 @@
 class AddDefaultValue < ActiveRecord::Migration[5.2]
   def change
-    change_column :tasks, :completed, :default, :false
+    change_column :tasks, :completed, :boolean, default: false
   end
 end
 
